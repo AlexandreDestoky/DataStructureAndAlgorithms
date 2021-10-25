b1608109a429c6edb615fa5f51841ffc78191f04
Destoky <adestoky@gmail.com>
2021-10-25 23:07:03 +0200

    Factorielle
    Avec fonction récursive

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Fibonacci
+//Fibonacci------------------------------------------------------
 const fibonacci = nbr => {
   const fib = [1, 1];
   for (let i = 0; i < nbr - 2; i++) {
@@ -9,14 +9,14 @@ const fibonacci = nbr => {
 
 // console.log(fibonacci(7));
 
-//2 plus petits nombres d'un tableau
+//2 plus petits nombres d'un tableau--------------------------------
 const twoSmallestNbr = tab => tab.sort((a, b) => a - b).slice(0, 2);
 
 const test = [85, 2, 35, 68, 7, 47];
 
 // console.log(twoSmallestNbr(test));
 
-// Trouver nombre itérations tableau partie test
+// Trouver nombre itérations tableau partie test------------------------------
 const personne = ["Jean", "Patrick", "Jean", "Jean", "Thomas", "Jean", "Jean"];
 
 const nbrInTab = (tab, mot) => {
@@ -28,7 +28,7 @@ const nbrInTab = (tab, mot) => {
 };
 // console.log(nbrInTab(personne,"Jean"));
 
-//Comparer si valeur en commun dans 2 tableau
+//Comparer si valeur en commun dans 2 tableau----------------------------
 const compare2Array = (tab1, tab2) => {
   tab1 = [...new Set(tab1)];
   tab2 = [...new Set(tab2)];
@@ -37,4 +37,12 @@ const compare2Array = (tab1, tab2) => {
 
 let testTab = ["a", "b", "c"];
 let testTab2 = ["d", "e", "f", "f", "cj"];
-console.log(compare2Array(testTab, testTab2));
+// console.log(compare2Array(testTab, testTab2));
+
+// Fonction Factorielle -----------------------------------
+const Factorielle = nbr => {
+  if (nbr === 1) return 1;
+  return nbr * Factorielle(nbr - 1);
+};
+
+console.log(Factorielle(7));
